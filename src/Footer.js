64ef010e2907841991004435178466c8dadd3cd5
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-21 18:33:12 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+export default function Footer() {
+  return (
+    <footer>
+      This page was coded by{" "}
+      <a href="https://github.com/smritisinha0801">Smriti Sinha</a>,{" "}
+      <a href="https://github.com/Shivika2934">Shivika Mittal</a> and{" "}
+      <a href="https://github.com/TShreya-27">Shreya Tiwari</a> and is
+      open-sourced on{" "}
+      <a href="https://github.com/Shivika2934/time-table">Github</a>
+    </footer>
+  );
+}
