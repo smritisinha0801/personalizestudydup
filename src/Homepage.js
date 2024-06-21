03af2d4e0e1e8ef1a4fc527b77f2905c699536c3
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-21 18:33:48 +0530

    Add files via upload

---
+++ src/Homepage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+export default function Homepage() {
+  return (
+    <div className="container header mt-4 text-center">
+      <p className="para">
+        Welcome to our personalized timetable generator! Our website is designed
+        to help you create the perfect study schedule tailored to your unique
+        needs for exam season. Simply enter a few basic details about your
+        subjects, including the number of days per week, subjects and their
+        credit hours, study hours per day, break intervals (in hours), and break
+        duration (in minutes). Our system will then generate a customized
+        timetable that covers your entire syllabus, making it an invaluable tool
+        for students in both schools and colleges. Start optimizing your study
+        time today with our easy-to-use timetable generator!
+      </p>
+    </div>
+  );
+}
